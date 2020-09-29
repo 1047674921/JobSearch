e1cdbee037b6d3b3beb34839e850563f75b57d9d
1047674921 <52452520+1047674921@users.noreply.github.com>
2020-09-29 17:56:44 +0800

    音乐播放器小程序列表渲染

--- 练习/音乐播放器/pages/index/index.js
+++ 练习/音乐播放器/pages/index/index.js
@@ -1,9 +1,44 @@
 //index.js
-
 Page({
   data: {
     tab:0,
     item:0,
+    list:[{
+      id:1,
+      name:'歌曲2',
+      singer: '佚名',
+      musicimage:'../image/timg.jpg'
+    },
+    {
+      id:2,
+      name:'歌曲2',
+      singer: '佚名',
+      musicimage:'../image/timg.jpg'
+    },
+    {
+      id:3,
+      name:'歌曲3',
+      singer: '佚名',
+      musicimage:'../image/timg.jpg'
+    },
+    {
+      id:4,
+      name:'歌曲4',
+      singer: '佚名',
+      musicimage:'../image/timg.jpg'
+    },
+    {
+      id:5,
+      name:'歌曲5',
+      singer: '佚名',
+      musicimage:'../image/timg.jpg'
+    },{
+      id:6,
+      name:'歌曲6',
+      singer: '佚名',
+      musicimage:'../image/timg.jpg'
+    }
+  ],
   },
   //事件处理函数
   changeItem: function(e) {
